5af622fa64c1550ed41d854c9183fa37d55828b4
Andres Rendo <andres.rendo@correo.unimet.edu.ve>
2022-10-24 16:31:12 -0400

    Creacion de clases

---
+++ Laberinto/src/Proyecto/Proyecto1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Proyecto;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author mannith
+ */
+public class Proyecto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    static JFrame ventana = new JFrame();
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ventana.setSize(400,400);
+        ventana.setTitle("Hacedor de laberintos 3000");
+        ventana.setVisible(true);
+        
+    }
+    
+}
