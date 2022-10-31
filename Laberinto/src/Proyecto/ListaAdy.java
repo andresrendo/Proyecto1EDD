78f316b5a0713e4796cf62b9219885e86a6aeb76
AndresGabrielGG <97445082+AndresGabrielGG@users.noreply.github.com>
2022-10-30 22:13:32 -0400

    Update ListaAdy.java

--- Laberinto/src/Proyecto/ListaAdy.java
+++ Laberinto/src/Proyecto/ListaAdy.java
@@ -1,13 +1,15 @@
 public class ListaAdy <T>{
-  Vertice primero;
-  Vertice ultimo;
+  Vertices primero;
+  Vertices ultimo;
+  int tamaño;
   public ListaAdy (){
     primero = ultimo = null;
+    tamaño = 0;
   }
-  public Vertice getPrimero() {
+  public Vertices getPrimero() {
         return primero;
     }
-    public void setPrimero(Vertice p) {
+    public void setPrimero(Vertices p) {
         this.primero = p;
     }
   
@@ -15,8 +17,9 @@ public class ListaAdy <T>{
     return primero == null;
   }
   
-  public insertar(T dato){
-    Vertice temp = Vertice(dato);
+  public void insertar( int dato, int peso){
+    Vertices temp = new Vertices(dato);
+    temp.setPeso(peso);
     if (primero == null){
       primero = ultimo = temp;
       }
@@ -24,12 +27,13 @@ public class ListaAdy <T>{
       ultimo.setvSiguiente(temp);
       ultimo = temp;
     }
+    tamaño++;
   }
   
-  public boolean contains(Vertice v){
-    Vertice aux = primero;
-    while (aux.getvSiguiente != null){
-      if aux.getIndice() == v.getIndice(){
+  public boolean contains(Vertices v){
+    Vertices aux = primero;
+    while (aux != null){
+      if (aux.getIndice() == v.getIndice()){
         return true;
       }
       aux = aux.getvSiguiente();
@@ -37,27 +41,40 @@ public class ListaAdy <T>{
     return false;
   }
   
-  public eliminar(T dato){
-  for(i = primero; i != null, i = primero.getvSiguiente()) {
-      if  (i.getIndice == dato){
-        if (i.getvAnterior != null && i.getvSiguiente() != null){
-          temp = i.getvAnterior();
-          temp.setSiguiente(i.getvSiguiente());
+  public void eliminar(int dato){
+  Vertices i = primero;
+  while(i != null) {
+      if  (i.getIndice() == dato){
+        if (i.getvAnterior() != null && i.getvSiguiente() != null){
+          Vertices temp = i.getvAnterior();
+          temp.setvSiguiente(i.getvSiguiente());
           i.getvSiguiente().setvAnterior(temp);
           
           }
-        else if (i.getvAnterior == null && i.getvSiguiente() != null){
+        else if (i.getvAnterior() == null && i.getvSiguiente() != null){
           i.getvSiguiente().setvAnterior(null);
           primero = i.getvSiguiente();
           }
-        else if (i.getvAnterior != null && i.getvSiguiente() == null){
+        else if (i.getvAnterior() != null && i.getvSiguiente() == null){
           i.getvAnterior().setvSiguiente(null);
           ultimo = i.getvAnterior();
           }
+        i.getvSiguiente();
       }
   }
+  tamaño--;
   }
+  
+  public int getTamaño(){
+      return tamaño;
+  }
+  
+  public void setTamaño(int n){
+      tamaño = n;
+  }
+
 
 
 
 }
+
